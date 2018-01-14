d734325d047f0360813622ec01d905bb17cf5f1f
luukspierings <lcpspier@avans.nl>
2018-01-14 21:41:34 +0100

    include polygon to tests

--- Framework/SDLFramework/SDLFramework/Polygon.cpp
+++ Framework/SDLFramework/SDLFramework/Polygon.cpp
@@ -1,11 +1,11 @@
+#include "stdafx.h"
 #include "Polygon.h"
 
 #include "Matrix.h"
 
 void Polygon::draw(Matrix* object)
 {
-	FWApplication* app = FWApplication::GetInstance();
-	app->SetColor(color);
+	FWApplication::GetInstance()->SetColor(color);
 
 	if (visibleAB) drawLine(object->values[A], object->values[B]);
 	if (visibleAC) drawLine(object->values[A], object->values[C]);
@@ -14,11 +14,10 @@ void Polygon::draw(Matrix* object)
 
 void Polygon::drawLine(vector<float> pointA, vector<float> pointB)
 {
-	FWApplication* app = FWApplication::GetInstance();
 
 	if ((pointA.size() > 3 && pointA[3] < 0.f) ||
 		(pointB.size() > 3 && pointB[3] < 0.f)) {
 
-		app->DrawLine(pointA[0], pointA[1], pointB[0], pointB[1]);
+		FWApplication::GetInstance()->DrawLine(pointA[0], pointA[1], pointB[0], pointB[1]);
 	}
 }
