141969b8b9a283710cb57801c299c81c03b23d94
Jonathan <jgimmink@avans.nl>
2018-01-14 14:09:56 +0100

    Added more tests

--- Framework/SDLFramework/UnitTests/MatrixTest.cpp
+++ Framework/SDLFramework/UnitTests/MatrixTest.cpp
@@ -3,6 +3,8 @@
 
 #include "../SDLFramework/Matrix.h"
 #include "../SDLFramework/TranslationMatrix.h"
+#include "../SDLFramework/RotationMatrix.h"
+#include "../SDLFramework/ScalingMatrix.h"
 #include "../SDLFramework/Vector.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -13,7 +15,7 @@ namespace UnitTests
 	{
 	public:
 
-		TEST_METHOD(MatrixCopy)
+		TEST_METHOD(Matrix_Copy)
 		{
 			Matrix matrix1;
 			matrix1.addValue({ 2.f, 3.f });
@@ -27,7 +29,7 @@ namespace UnitTests
 			Assert::AreEqual(matrix1.values[1][1], matrix2.values[1][1]);
 		}
 
-		TEST_METHOD(MatrixTranslation)
+		TEST_METHOD(Matrix_Translation)
 		{
 			Vector vector({ 2.f, 2.f });
 
@@ -42,6 +44,123 @@ namespace UnitTests
 			Assert::AreEqual(matrix1.values[1][0], 6.f);
 			Assert::AreEqual(matrix1.values[0][1], 5.f);
 			Assert::AreEqual(matrix1.values[1][1], 6.f);
+		}
+
+		TEST_METHOD(Matrix_Rotation)
+		{
+			Vector vector({0.f, 1.f, 0.f});
+
+			Matrix matrix1;
+			matrix1.addValue({3.f, 3.f, 3.f});
+			matrix1.addValue({4.f, 4.f, 4.f});
+
+			RotationMatrix rotationMatrix(90, vector);
+
+			matrix1 = rotationMatrix * matrix1;
+
+			Assert::AreEqual(matrix1.values[0][0], 3.f, 0.001f);
+			Assert::AreEqual(matrix1.values[0][1], 3.f, 0.001f);
+			Assert::AreEqual(matrix1.values[0][2], -3.f, 0.001f);
+
+			Assert::AreEqual(matrix1.values[1][0], 4.f, 0.001f);
+			Assert::AreEqual(matrix1.values[1][1], 4.f, 0.001f);
+			Assert::AreEqual(matrix1.values[1][2], -4.f, 0.001f);
+		}
+
+		TEST_METHOD(Matrix_Scaling)
+		{
+			Vector vector({0.f, 1.f, 0.f});
+
+			Matrix matrix1;
+			matrix1.addValue({3.f, 3.f, 3.f});
+			matrix1.addValue({4.f, 4.f, 4.f});
+
+			ScalingMatrix scalingMatrix(3, 5);
+
+			matrix1 = scalingMatrix * matrix1;
+
+			Assert::AreEqual(matrix1.values[0][0], 15.f, 0.001f);
+			Assert::AreEqual(matrix1.values[0][1], 15.f, 0.001f);
+			Assert::AreEqual(matrix1.values[0][2], 15.f, 0.001f);
+
+			Assert::AreEqual(matrix1.values[1][0], 20.f, 0.001f);
+			Assert::AreEqual(matrix1.values[1][1], 20.f, 0.001f);
+			Assert::AreEqual(matrix1.values[1][2], 20.f, 0.001f);
+		}
+
+		TEST_METHOD(Matrix_Scaling_Translation)
+		{
+			Vector vectorTranslation({2.f, 2.f, 2.f});
+
+			Matrix matrix1;
+			matrix1.addValue({3.f, 3.f, 3.f});
+			matrix1.addValue({4.f, 4.f, 4.f});
+
+			ScalingMatrix scalingMatrix(3, 5);
+			TranslationMatrix translationMatrix(vectorTranslation);
+			
+			matrix1 = scalingMatrix * matrix1;
+			matrix1 = translationMatrix * matrix1;
+
+			Assert::AreEqual(matrix1.values[0][0], 17.f);
+			Assert::AreEqual(matrix1.values[0][1], 17.f);
+			Assert::AreEqual(matrix1.values[0][2], 17.f);
+
+			Assert::AreEqual(matrix1.values[1][0], 22.f);
+			Assert::AreEqual(matrix1.values[1][1], 22.f);
+			Assert::AreEqual(matrix1.values[1][2], 22.f);
+		}
+
+		TEST_METHOD(Matrix_Rotation_Translation)
+		{
+			Vector vectorRotation({0.f, 1.f, 0.f});
+			Vector vectorTranslation({2.f, 2.f, 2.f});
+
+
+			Matrix matrix1;
+			matrix1.addValue({3.f, 3.f, 3.f});
+			matrix1.addValue({4.f, 4.f, 4.f});
+
+			TranslationMatrix translationMatrix(vectorTranslation);
+			RotationMatrix rotationMatrix(90, vectorRotation);
+
+			matrix1 = rotationMatrix * matrix1;
+			matrix1 = translationMatrix * matrix1;
+
+			Assert::AreEqual(matrix1.values[0][0], 5.f, 0.001f);
+			Assert::AreEqual(matrix1.values[0][1], 5.f, 0.001f);
+			Assert::AreEqual(matrix1.values[0][2], -1.f, 0.001f);
+
+			Assert::AreEqual(matrix1.values[1][0], 6.f, 0.001f);
+			Assert::AreEqual(matrix1.values[1][1], 6.f, 0.001f);
+			Assert::AreEqual(matrix1.values[1][2], -2.f, 0.001f);
+		}
+
+		TEST_METHOD(Matrix_Rotation_Scaling_Translation)
+		{
+			Vector vectorRotation({0.f, 1.f, 0.f});
+			Vector vectorTranslation({2.f, 2.f, 2.f});
+
+
+			Matrix matrix1;
+			matrix1.addValue({3.f, 3.f, 3.f});
+			matrix1.addValue({4.f, 4.f, 4.f});
+
+			TranslationMatrix translationMatrix(vectorTranslation);
+			RotationMatrix rotationMatrix(90, vectorRotation);
+			ScalingMatrix scalingMatrix(3, 4);
+
+			matrix1 = rotationMatrix * matrix1;
+			matrix1 = scalingMatrix * matrix1;
+			matrix1 = translationMatrix * matrix1;
+
+			Assert::AreEqual(matrix1.values[0][0], 14.f, 0.001f);
+			Assert::AreEqual(matrix1.values[0][1], 14.f, 0.001f);
+			Assert::AreEqual(matrix1.values[0][2], -10.f, 0.001f);
+
+			Assert::AreEqual(matrix1.values[1][0], 18.f, 0.001f);
+			Assert::AreEqual(matrix1.values[1][1], 18.f, 0.001f);
+			Assert::AreEqual(matrix1.values[1][2], -14.f, 0.001f);
 		}
 	};
 }
