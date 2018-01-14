fcb43280b863d78e620aef6345bda816eb6712ac
Jonathan <jgimmink@avans.nl>
2018-01-14 21:06:28 +0100

    Fixed pulsing scale + added random squares in space

--- Framework/SDLFramework/SDLFramework/Square.cpp
+++ Framework/SDLFramework/SDLFramework/Square.cpp
@@ -26,7 +26,7 @@ Square::Square(Vector position, float size, Color color)
 	addValue({ xS,y,zS });
 
 	// back middle
-	addValue({ x + (size / 2),y+(size/2),z });
+	//addValue({ x + (size / 2),y+(size/2),z });
 
 
 	// front lines
